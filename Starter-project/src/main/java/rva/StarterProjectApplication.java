8718cdbb5fce1228603b51ee2de927f6094dae6b
burazen-sudo <56589469+burazen-sudo@users.noreply.github.com>
2021-04-23 10:36:06 +0200

    Commit za 1 prakticni test

--- Starter-project/src/main/java/rva/StarterProjectApplication.java
+++ Starter-project/src/main/java/rva/StarterProjectApplication.java
@@ -1,9 +1,13 @@
 package rva;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-// GitHub Classroom Invite Link --> https://classroom.github.com/a/PgzNRc2E
 
 @SpringBootApplication
 public class StarterProjectApplication {
@@ -11,5 +15,6 @@ public class StarterProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StarterProjectApplication.class, args); 
 	}
-
+	
+	
 }
